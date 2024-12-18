ae1909c12137e3d8bc7b2d3e0a523606239aa061
LAPTOPP5 <125674717+LAPTOPP5@users.noreply.github.com>
2024-12-18 23:17:04 +0530

    initial commit on new branch

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React,{useEffect, useState} from 'react';
-import ChatRoom from './components/ChatRoom';
-import Login from './components/Login';
-import { BrowserRouter as Router,Routes,Route, Navigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import ChatRoom from "./components/ChatRoom";
+import Login from "./components/Login";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
+  const [user, setUser] = useState(() => {
+    const savedUser = sessionStorage.getItem("user");
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
 
-  const [user, setUser] = useState(
-    ()=>{
-      const savedUser=sessionStorage.getItem('user'); 
-      return savedUser ? JSON.parse(savedUser):null;
-    }
-  );
-  
-  useEffect(()=>{
-     user ? sessionStorage.setItem('user',JSON.stringify(user)):sessionStorage.removeItem('user');
-  },[user]);
+  useEffect(() => {
+    user
+      ? sessionStorage.setItem("user", JSON.stringify(user))
+      : sessionStorage.removeItem("user");
+  }, [user]);
 
   return (
     <div className="App">
-
-       <div style={{ fontFamily: "Arial, sans-serif", textAlign: "center" }}>
-          
-          <Router>
-            <Routes>
-              <Route
-                 path='/'
-                 element={user ? <Navigate to='/chatroom'/>:<Login setUser={setUser}/>}
-                 />
-              <Route
-                 path='/chatroom'
-                 element={<ChatRoom User={user}/>}/>   
-            </Routes>
-          </Router>
-    </div>
-
+      <div style={{ fontFamily: "Arial, sans-serif", textAlign: "center" }}>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                user ? <Navigate to="/chatroom" /> : <Login setUser={setUser} />
+              }
+            />
+            <Route path="/chatroom" element={<ChatRoom User={user} />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
